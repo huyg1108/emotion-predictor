a30665fcbae52fdece5cedb934b50effc4593ae2
huyg1108 <67747576+huyg1108@users.noreply.github.com>
2022-03-11 09:21:06 +0700

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -5,8 +5,8 @@ from transformers import pipeline
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 candidate_labels = ["happy", "sad", "angry","anxious","bored", "horrified", "confused", "excited"]
 
-st.title("Demo app")
-input = st.text_input("Write what you are feeling:")
+st.title("Emotion App")
+input = st.text_input("Write what you are thinking:")
 
 if len(input) > 0:
 	classifier = pipeline("zero-shot-classification", device=0)
